b7558700cb2b28097239b25262a1e9794254ac6e
ndadalto16 <44184759+ndadalto16@users.noreply.github.com>
2020-02-21 23:01:26 -0800

    Updated Phrase

--- map.py
+++ map.py
@@ -2,7 +2,8 @@ import speech_recognition as sr
 
 
 r = sr.Recognizer()
-r.energy_threshold = 500
+
+WORDS = ["my name is", "What is your name I'm", "nice to meet you", "hi I'm", "What's your name", "please meet", ]
 
 with sr.Microphone() as source:
     print('Speak Anything')
@@ -10,6 +11,9 @@ with sr.Microphone() as source:
 
     try:
         text = r.recognize_google(audio)
+        if any(word in WORDS for word in WORDS):
+            print('PASS')
         print('You said : {}'.format(text))
     except:
         print("Sorry don't know what you said")
+
